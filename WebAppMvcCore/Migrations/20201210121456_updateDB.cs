1761c1c06cbec967b13ed171e83addd7b8f3edc2
joaomachado46 <joaopcmachado46@gmail.com>
2020-12-10 12:20:10 +0000

    UpdateDB + created Models

---
+++ WebAppMvcCore/Migrations/20201210121456_updateDB.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppMvcCore.Migrations
+{
+    public partial class updateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    BaseSalary = table.Column<double>(nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    DepartmentIdId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sellers_Department_DepartmentIdId",
+                        column: x => x.DepartmentIdId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    SellerIdId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SalesRecords_Sellers_SellerIdId",
+                        column: x => x.SellerIdId,
+                        principalTable: "Sellers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesRecords_SellerIdId",
+                table: "SalesRecords",
+                column: "SellerIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sellers_DepartmentIdId",
+                table: "Sellers",
+                column: "DepartmentIdId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesRecords");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+        }
+    }
+}
